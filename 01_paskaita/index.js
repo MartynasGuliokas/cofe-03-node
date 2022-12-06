cbc9be06c6c126df957ed91ae10fdcb9b4a14bee
Martynas Guliokas <martynasguliokas@Martynass-MacBook-Pro.local>
2022-12-06 15:32:09 +0200

    Atliktas darbas

---
+++ 01_paskaita/index.js
@@ -0,0 +1,10 @@
+// console.log("Helllo Martynas");
+
+const casual = require("casual");
+
+function number(max) {
+    return Math.floor((Math.random() * 10) + 1);
+}
+
+console.log(casual.city);
+console.log(number(10));
