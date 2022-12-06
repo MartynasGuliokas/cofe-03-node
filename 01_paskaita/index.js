0917be7b49f0eb6dea016f7c8f33bb669d1e36dd
Martynas Guliokas <martynasguliokas@Martynass-MacBook-Pro.local>
2022-12-06 20:21:53 +0200

    Papildytas namu darbas

--- 01_paskaita/index.js
+++ 01_paskaita/index.js
@@ -1,10 +1,25 @@
 // console.log("Helllo Martynas");
 
 const casual = require("casual");
+console.log(casual.city);
 
-function number(max) {
+
+function number() {
     return Math.floor((Math.random() * 10) + 1);
 }
+console.log(number());
 
-console.log(casual.city);
-console.log(number(10));
+
+const randomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+console.log(randomNumber(1, 10));
+
+
+console.log(`${casual.name_prefix} ${casual.first_name} ${casual.last_name}`)
+
+
+const name = "Rokas";
+const surname = "Andreikenas";
+const fullName = name + " " + surname; // bad
+const newFullName = `${name} ${surname}` // good
